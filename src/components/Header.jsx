77a1fb6d368ae3ced4f59eeee488055c4c0934c0
makoto2025 <aratamakoto2025@gmail.com>
2026-01-15 20:07:48 -0800

    changed header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 // 1. Mako
 const Header = ({ name, tagline }) => (
- <header className="header">
-    <h1 className="title">{name}</h1>
-    <div className="divider"></div>
-    <p className="tagline">{tagline}</p>
+ <header className="header elegant">
+    <div className="border-frame">
+      <h1 className="title">{name}</h1>
+      <div className="divider-icon">✦</div> 
+      <p className="tagline">{tagline}</p>
+    </div>
   </header>
 );
 
