acdf99150703edd802a8623d7703382ac1e5b5b3
jamie <jamiejy927@gmail.com>
2026-01-15 18:57:12 -0800

    first commit

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 // jamie
 const MenuItem = ({ name, price, description, tags }) => (
-  <h1>MenuItem</h1>
+  <div className="menu-item">    <div className="item-main">      <h3 className="item-name">{name}</h3>      <span className="item-price">${price}</span>    </div>    <p className="item-desc">{description}</p>    {tags && (      <div className="tag-container">        {tags.map(tag => <span key={tag} className="tag">{tag}</span>)}      </div>    )}  </div>
 );
 
 export default MenuItem
